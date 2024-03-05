49df595221d01620ffe8be4836107fca01114f7c
Debajyotijana03 <79994808+Debajyotijana03@users.noreply.github.com>
2024-03-05 12:16:22 +0530

    Update smallKmeans.py

--- smallKmeans.py
+++ smallKmeans.py
@@ -46,9 +46,8 @@ class MRKMeans(MRJob):
 
         yield centroid_id, new_centroid
 
-    @property
     def steps(self):
         return [self.mr(mapper=self.mapper, reducer=self.reducer)] * self.max_iterations
 
 if __name__ == '__main__':
-    MRKMeans.run()
+    MRKMeans().run()
