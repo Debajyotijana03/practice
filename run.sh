d6b8814bbf0128754698f42c13d6350b6da528a6
Debajyotijana03 <79994808+Debajyotijana03@users.noreply.github.com>
2024-05-05 12:39:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,35 +3,30 @@
 i=1
 
 while :
-
 do
+    hadoop jar hadoop-3.2.4/share/hadoop/tools/lib/hadoop-streaming-3.2.4.jar \
+        -file /home/debajyoti/Downloads/Map/centroids.txt \
+        -file /home/debajyoti/Downloads/Map/mapper.py \
+        -mapper /home/debajyoti/Downloads/Map/mapper.py \
+        -file /home/debajyoti/Downloads/Map/reducer.py \
+        -reducer /home/debajyoti/Downloads/Map/reducer.py \
+        -input /testMapReduce/dataset.txt \
+        -output /testMapReduce/mapreduce-output$i
 
-	hadoop jar hadoop-3.2.4/share/hadoop/tools/lib/hadoop-streaming-3.2.4.jar -file /home/debajyoti/Downloads/Map/centroids.txt -file /home/debajyoti/Downloads/Map/mapper.py -mapper /home/debajyoti/Downloads/Map/mapper.py -file /home/debajyoti/Downloads/Map/reducer.py -reducer /home/debajyoti/Downloads/Map/reducer.py -input /testMapReduce/dataset.txt -output /testMapReduce/mapreduce-output$i
+    rm -f centroids1.txt
 
-	rm -f centroids1.txt
+    hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids1.txt
 
-	hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids1.txt
+    seeiftrue=$(python3 reader.py)
 
-	seeiftrue=`python3 reader.py`
+    if [ $seeiftrue = 1 ]; then
+        rm centroids.txt
+        hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
+        break
+    else
+        rm centroids.txt
+        hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
+    fi
 
-	if [ $seeiftrue = 1 ]
-
-	then
-
-		rm centroids.txt
-
-		hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
-
-		break
-
-	else
-
-		rm centroids.txt
-
-		hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
-
-	fi
-
-	i=$((i+1))
-
+    i=$((i+1))
 done
